1bffbb48b06881b6ffcdd304dfcf989193733aef
baba_s <shotababa1206@gmail.com>
2019-09-29 20:18:35 +0900

    first commit

---
+++ Editor/ApplicationPathWindow.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.IO;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	internal sealed class ApplicationPathWindow : EditorWindow
+	{
+		private Vector2 m_scrollPos;
+
+		[MenuItem( "Window/Application Path Window" )]
+		public static void Init()
+		{
+			GetWindow<ApplicationPathWindow>( "Application Path Window" );
+		}
+
+		private void OnGUI()
+		{
+			m_scrollPos = EditorGUILayout.BeginScrollView( m_scrollPos );
+
+			EditorGUILayout.LabelField( "Application", EditorStyles.boldLabel );
+
+			var drawer = new Drawer( 224 );
+
+			drawer.Draw( "dataPath", Application.dataPath );
+			drawer.Draw( "consoleLogPath", Application.consoleLogPath );
+			drawer.Draw( "persistentDataPath", Application.persistentDataPath );
+			drawer.Draw( "streamingAssetsPath", Application.streamingAssetsPath );
+			drawer.Draw( "temporaryCachePath", Application.temporaryCachePath );
+
+			EditorGUILayout.Space();
+			EditorGUILayout.LabelField( "EditorApplication", EditorStyles.boldLabel );
+
+			drawer.Draw( "applicationContentsPath", EditorApplication.applicationContentsPath );
+			drawer.Draw( "applicationPath", EditorApplication.applicationPath );
+
+			EditorGUILayout.Space();
+			EditorGUILayout.LabelField( "InternalEditorUtility", EditorStyles.boldLabel );
+
+			drawer.Draw( "unityPreferencesFolder", InternalEditorUtility.unityPreferencesFolder );
+			drawer.Draw( "GetCrashReportFolder", InternalEditorUtility.GetCrashReportFolder() );
+			drawer.Draw( "GetEditorAssemblyPath", InternalEditorUtility.GetEditorAssemblyPath() );
+			drawer.Draw( "GetEngineAssemblyPath", InternalEditorUtility.GetEngineAssemblyPath() );
+			drawer.Draw( "GetEngineCoreModuleAssemblyPath", InternalEditorUtility.GetEngineCoreModuleAssemblyPath() );
+
+			EditorGUILayout.EndScrollView();
+		}
+
+		private sealed class Drawer
+		{
+			private readonly int m_width;
+
+			public Drawer( int width ) => m_width = width;
+
+			public void Draw( string label, string text )
+			{
+				using ( new EditorGUILayout.HorizontalScope() )
+				{
+					EditorGUILayout.LabelField( label, GUILayout.Width( m_width ) );
+
+					if ( GUILayout.Button( "Copy", GUILayout.Width( 40 ) ) )
+					{
+						EditorGUIUtility.systemCopyBuffer = text;
+					}
+
+					if ( GUILayout.Button( "Open", GUILayout.Width( 40 ) ) )
+					{
+						var extension = Path.GetExtension( text );
+						var isFolder  = string.IsNullOrWhiteSpace( extension );
+						var path      = isFolder ? text : Path.GetDirectoryName( text ).Replace( "\\", "/" );
+
+						try
+						{
+							System.Diagnostics.Process.Start( path );
+						}
+						catch ( Win32Exception e )
+						{
+							Debug.Log( e.Message );
+						}
+					}
+
+					EditorGUILayout.TextField( text );
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Uni Application Path Window
+
+Application.dataPath などで取得できる文字列を一覧で確認できるエディタ拡張
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-application-path-window.svg)](https://github.com/baba-s/uni-application-path-window/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-application-path-window": "https://github.com/baba-s/uni-application-path-window.git",
+```
+
+manifest.json に上記の記述を追加します  
+
+## 使い方
+
+![](https://cdn-ak.f.st-hatena.com/images/fotolife/b/baba_s/20190929/20190929201704.png)  
+
+Unity メニューの「Window > Application Path Window」を選択すると  
+
+![](https://cdn-ak.f.st-hatena.com/images/fotolife/b/baba_s/20190929/20190929201522.png)  
+
+Application.dataPath などの文字列を確認できます  
+クリップボードにコピーしたり、該当フォルダを開くこともできます  
